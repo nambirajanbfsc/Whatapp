c8386dd68fac6d7e826e9bd34bc229f275fe3957
Rahul Bhati <rb083380@gmail.com>
2023-08-04 11:07:29 +0530

    add chatbot and bulk msg page

--- js/script.js
+++ js/script.js
@@ -83,6 +83,110 @@ function selectAccount() {
   }
 }
 
+// auto responder acc select
+function selectAutoAcc() {
+  var x = document.getElementById("mySelect").value;
+  if (x === '0') {
+    document.getElementById("showData").innerHTML = "<center><img src='empty2.png' alt='empty' style='display: flex;align-items: center;justify-content: center;width:30%;'></center>"
+  } else {
+    document.getElementById("showData").innerHTML = `
+    <div class="card h-100">
+                                   <div class="card-header">
+                                        Autoresponder for Neha
+                                   </div>
+                                   <div class="card-body">
+                                        <form method="post" action="">
+                                             <!-- status radio -->
+                                             <p class="labelish">Status</p>
+                                             <div id="paymentContainer" name="paymentContainer" class="paymentOptions">
+                                                  <div id="payCC" class="floatBlock">
+                                                       <label for="paymentCC"> <input id="paymentCC" name="status" type="radio" value="enable" />Enable
+                                                       </label>
+                                                  </div>
+                                                  <div id="payInvoice" class="floatBlock">
+                                                       <label for="paymentInv"> <input id="paymentInv"
+                                                                 name="status" type="radio" value="disable" checked/>
+                                                            Disable</label>
+                                                  </div>
+                                             </div>
+                                             <!-- sent to radio btn -->
+                                             <p class="labelish">Sent to</p>
+                                             <div id="paymentContainer" name="paymentContainer" class="paymentOptions">
+                                                  <div id="payCC" class="floatBlock">
+                                                       <label for="paymentCC"> <input id="paymentCC" name="sent" type="radio" value="all" checked/>All
+                                                       </label>
+                                                  </div>
+                                                  <div id="payInvoice" class="floatBlock">
+                                                       <label for="paymentInv"> <input id="paymentInv"
+                                                                 name="sent" type="radio" value="disable" />
+                                                            Individual</label>
+                                                  </div>
+                                                  <div id="payInvoice" class="floatBlock">
+                                                       <label for="paymentInv"> <input id="paymentInv"
+                                                                 name="sent" type="radio" value="disable" />
+                                                            Group</label>
+                                                  </div>
+                                             </div><br>
+
+                                             <!-- tab bar -->
+                                             <ul class="nav nav-tabs" id="myTab" role="tablist">
+                                                  <li class="nav-item" role="presentation">
+                                                  <button class="nav-link active" id="home-tab" data-bs-toggle="tab" data-bs-target="#home" type="button" role="tab" aria-controls="home" aria-selected="true">Text & Media</button>
+                                                  </li>
+                                                  <li class="nav-item" role="presentation">
+                                                  <button class="nav-link" id="profile-tab" data-bs-toggle="tab" data-bs-target="#profile" type="button" role="tab" aria-controls="profile" aria-selected="false">Buttons</button>
+                                                  </li>
+                                                  <li class="nav-item" role="presentation">
+                                                  <button class="nav-link" id="contact-tab" data-bs-toggle="tab" data-bs-target="#contact" type="button" role="tab" aria-controls="contact" aria-selected="false">List Media</button>
+                                                  </li>
+                                             </ul>
+                                             <div class="tab-content" id="myTabContent">
+                                                  <div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">
+                                                       <p style="font-size: 15px;font-weight: 600;padding-top: 10px;">Media file</p>
+                                                       <div class="upload-btn-wrapper">
+                                                            <button class="upload_btn">Upload a file</button>
+                                                            <input type="file" name="myfile" />
+                                                       </div>
+                                                       <p style="font-size: 15px;font-weight: 600;padding-top: 10px;">Caption</p>
+                                                        <textarea name="the-textarea" id="the-textarea" maxlength="300" placeholder="Start Typin..."autofocus></textarea>
+                                                        <button class="btn btn-primary"><i class="fas fa-save"></i> Save</button>
+                                                  </div>
+                                                  <div class="tab-pane fade" id="profile" role="tabpanel" aria-labelledby="profile-tab">...</div>
+                                                  <div class="tab-pane fade" id="contact" role="tabpanel" aria-labelledby="contact-tab">...</div>
+                                             </div><br>
+
+                                             <!-- mob num card -->
+                                             <div class="card h-100">
+                                                  <div class="container-fluid p-2">
+                                                       <div class="form-group">
+                                                            <label for="min">Resubmit message only after (minute)</label>
+                                                            <select class="form-control" id="min" style="margin-top:10px;" >
+                                                                 <option>1</option>
+                                                                 <option>2</option>
+                                                                 <option>3</option>
+                                                                 <option>4</option>
+                                                                 <option>5</option>
+                                                            </select>
+                                                       </div><br>
+                                                       <div class="form-group">
+                                                            <div class="form-group">
+                                                                 <label for="tel">Except contacts</label>
+                                                                 <input type="tel" class="form-control" style="margin-top:10px;" id="tel" placeholder="Contacts" required>
+                                                                 <p style="font-size: 12px;color: rgb(182, 186, 186);margin-top: 5px;">Validate exapmle: 841234567890, 840123456789</p>
+                                                            </div>
+                                                       </div>
+                                                  </div>
+                                             </div><br>
+                                             <hr class="dropdown-divider" />
+                                             <!-- submit button -->
+                                             <button class="btn btn-success" type='submit'><i class="far fa-paper-plane"></i> submit</button>
+                                        </form>
+                                   </div>
+                              </div>
+     `;
+  }
+}
+
 // $(document).ready(function(){
 //   $("#profile").click(function(){
 //     alert("prfile:");
